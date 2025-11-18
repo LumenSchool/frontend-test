3f3061780399de2363e3a059351fb54c5df395e8
Victor8785 <victor117test@gmail.com>
2025-11-18 22:14:56 +0300

    backend querys

---
+++ chunk-52QCCMRP.js
@@ -0,0 +1,1 @@
+var e={production:!0,apiUrl:"https://lumen-backend.up.railway.app",debug:!1,version:"1.0.0"};export{e as a};
--- chunk-VVJYJCDW.js
+++
@@ -1,1 +0,0 @@
-var o={production:!0,apiUrl:`${window.location.protocol}//${window.location.host}`,debug:!1,version:"1.0.0"};export{o as a};
